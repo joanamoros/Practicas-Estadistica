4ddbdbea95d7d2aa68a7be2705a9b76c332cd379
johan023 <89706763+johan023@users.noreply.github.com>
2024-07-11 19:19:55 +0200

    Create variables-aleatorias-continuas.R

---
+++ variables-aleatorias-continuas.R
@@ -0,0 +1,10 @@
+# Ejercicio 1: Supóngase que la concentración que cierto contaminante se encuentra distribuida de manera uniforme en el intervalo de 0 a 20 partes de millón. 
+# Si se considera tóxica unaconcentración de 8 o más:
+# a) ¿Cuál es la probabilidad de que al tomarse una muestra la concentración de estasea tóxica? [Resultado: 0.60]
+1 - punif(8,0,20)
+# b) Calcula la concentración media y su variabilidad.
+#     E(x) <- (0+20)/2; E(x)= 10
+#     V(x) <- (20-0)^2 / 12; V(x)= 33.3333
+#     DT(x) <- sqrt(V(x)); DT(x)= 5.7735
+
+
