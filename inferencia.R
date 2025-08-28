b72d06110d2db0b394c0c9c3ba96bad15e60a707
joanamoros <89706763+joanamoros@users.noreply.github.com>
2025-08-28 11:29:28 +0200

    Create inferencia.R

---
+++ inferencia.R
@@ -0,0 +1,41 @@
+Ejercicio 1
+1. Calcula la probabilidad P(T ≤ 2.57) T~t5
+pt(2.5, 5)
+## 0.972755
+2. Calcula el valor de t que acumula una probabilidad de 0.975, T~t5
+qt(0.975,5)
+## 2.570582
+3. Calcula las siguientes probabilidades
+a) P(T ≥ 2.57) T~t5
+1 - pt(2.57,5)
+## 0.02501766
+b) P(T ≤ 4.14) T~t10
+pt(4.14, 10)
+## 0.9989941
+c) P(T ≤ 2.3) T~t12
+pt(2.3,12)
+## 0.9799012
+d) P(T ≥ 2.3) T~t20
+1 - pt(2.3,20)
+## 0.01617371
+4. Calcula los valores de t que acumulan las siguientes probabilidades:
+a) T~t10, ¿Qué valor de t acumula una prob. de 0.999?
+> qt(0.999,10)
+[1] 4.1437
+b) T~t12, ¿Qué valor de t acumula una prob. de 0.85?
+> qt(0.85,12)
+[1] 1.083211
+c) T~t20, ¿Qué valor de t acumula una prob. de 0.75?
+> qt(0.75,20)
+[1] 0.6869545
+
+
+
+
+
+
+
+
+
+
+
